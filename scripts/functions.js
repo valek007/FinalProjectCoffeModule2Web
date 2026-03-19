7d003d92ecda237cb701463a4eb3b8c22ed3f03d
Maxim Loginovskiy <frakiec3600@gmail.com>
2026-03-19 22:43:35 +0500

    add types

--- scripts/functions.js
+++ scripts/functions.js
@@ -2,6 +2,7 @@ let myNumber = 6;
 let myString = 'Alice';
 let myBoolean = true;
 let myArray = [];
+const coordinates = [4, 'alice'];
 let myArra2 = [];
 myArra2.push(4);
 myArra2.push(7);
@@ -11,6 +12,69 @@ function plusAB(a, b) {
 function minusAB(a, b) {
     return a - b;
 }
+function logNumber(a, b) {
+    if (b) {
+        console.log(a + b);
+    }
+    console.log(a);
+}
+async function returnPromise(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    }
+    catch (err) {
+        err.message;
+    }
+}
+const multiply = (x, y) => x * y;
 myString.toLowerCase();
-plusAB(7, 8);
+myNumber = plusAB(7, 8);
+logNumber(5);
+minusAB(3, 1);
+console.log(logNumber(6, 8));
+let admin = {
+    name: 'Petr',
+    age: 30,
+    access: 3,
+};
+admin.name = 'Aizhan';
+let id = 0;
+let email;
+if (email) {
+    email.toUpperCase();
+}
+id = 87;
+id = 'user87';
+id = false;
+// логическое или
+console.log(id || myNumber);
+function parseObject(data) {
+    if (!data) {
+        return 'data is empty';
+    }
+    const valuesArr = Object.values(data);
+    data.email?.toLowerCase();
+    data.name.toLowerCase();
+    return valuesArr;
+}
+let status = 'success';
+function handleRequest(status) {
+    switch (status) {
+        case 'success':
+            console.log('all done');
+            break;
+        case 'error':
+            console.log('some error');
+            break;
+        default:
+            break;
+    }
+}
+let url;
+url = 'localhost:3000';
+if (typeof url === 'string') {
+    url.toLowerCase();
+}
 export {};
--- ts-scripts/functions.ts
+++ ts-scripts/functions.ts
@@ -3,7 +3,7 @@ let myString:string = 'Alice'
 let myBoolean: boolean = true
 
 let myArray: number[] = []
-const coordinates: [number, string] = [4, 'alice']
+const coordinates: readonly [number, string] = [4, 'alice']
 let myArra2: Array<number> = []
 
 
@@ -18,9 +18,32 @@ function minusAB(a:number, b:number) {
    return a - b
 }
 
-function logNumber(a:number): void {
-    console.log(a);
+function logNumber(a:number, b?: number): void {
+   if (b) {
+    console.log(a + b);
+   }
+   console.log(a);
 }
+
+async function returnPromise(url: string) :Promise<any>  {
+   try {
+      const response = await fetch(url)
+
+     const data = await response.json()
+     
+     return data
+   } catch (err){
+      type ErrorType = {
+         message: string
+      }
+
+      (err as { message: string }).message
+   }
+}
+
+type multiplyType = (x: number, y: number) => number
+
+const multiply: multiplyType = (x, y) =>  x * y
 
 myString.toLowerCase()
 
@@ -28,4 +51,94 @@ myString.toLowerCase()
 myNumber = plusAB(7, 8)
 logNumber(5)
 minusAB(3, 1)
+console.log(logNumber(6, 8));
+
+interface IUser {
+   name: string;
+   age: number;
+   email?: string;
+}
+
+type UserType = {
+   name: string;
+   age: number;
+   email?: string;
+}
+
+type EmployeType = {
+   profession: string;
+   stage: number;
+} & UserType
+
+type AccessType = 1 | 2 | 3
+
+type AdminType = {
+   readonly access: AccessType
+} & UserType
+
+let admin: AdminType = {
+  name: 'Petr',
+  age: 30,
+  access: 3,
+}
+
+admin.name = 'Aizhan'
+
+let id: number | string | boolean = 0
+let email: string | undefined
+
+if(email){
+   email.toUpperCase()
+}
+
+id = 87
+id = 'user87'
+id = false
+
+// логическое или
+console.log(id || myNumber);
+
+function parseObject(data:UserType | null) {
+   if(!data) {
+      return 'data is empty'
+   }
+
+   const valuesArr = Object.values(data)
+
+   data.email?.toLowerCase()
+   data.name.toLowerCase()
+
+   return valuesArr
+}
+
+type StatusType = 'error' | 'success' | 'pending'
+
+let status: StatusType = 'success'
+
+function handleRequest(status: StatusType) {
+  switch (status) {
+   case 'success':
+      console.log('all done');
+      break;
+
+   case 'error':
+      console.log('some error');
+      break;
+  
+   default:
+      break;
+  }
+}
+
+let url: unknown
+
+url = 'localhost:3000'
+
+if(typeof url === 'string') {
+  url.toLowerCase()
+}
+
+
+
+
 
